863c79683b20c2628e7cd6e3f873393724683764
janmayos <jangasca5@gmail.com>
2024-11-10 10:50:54 -0600

    Se agrega procesamiento del dataset y se castean los tipos de datos

--- datasets.py
+++ datasets.py
@@ -1,3 +1,5 @@
+import re
+import math
 
 def crear_matriz_linea_linea_archivo(archivo):
     matriz_file = []
@@ -8,11 +10,34 @@ def crear_matriz_linea_linea_archivo(archivo):
             matriz_file.append(line.strip())
     return matriz_file
 
-def imprimir_linea_matriz(matriz):
+def imprimir_linea_matriz(matriz_file):
     for line in matriz_file:
         print(line)
+
+def procesar_data(matriz_file):
+    matriz_process = []
+    for line in matriz_file:
+        if line.find(",") != -1:
+            datosline = line.split(",")
+            datoslinecast = []
+            for dato in datosline:
+                if re.match("[0-9]+.[0-9]*",dato):
+                    datoslinecast.append(float(dato))
+                else:
+                    datoslinecast.append(dato)
+            matriz_process.append((datoslinecast))
+    return matriz_process
+
+def separar_caracteristicas_clases(matri_process):
+    caracteristicas = [row[:-1] for row in matri_process]  
+    clases = [row[-1] for row in matri_process]   
+    return caracteristicas,clases
 
 if __name__ == "__main__":
     matriz_file = crear_matriz_linea_linea_archivo('iris/bezdekIris.data')
     imprimir_linea_matriz(matriz_file)
-    
+    matriz_process = procesar_data(matriz_file)
+    imprimir_linea_matriz(matriz_process)
+    caracteristicas,clases = separar_caracteristicas_clases(matriz_process)
+    imprimir_linea_matriz(caracteristicas)
+
